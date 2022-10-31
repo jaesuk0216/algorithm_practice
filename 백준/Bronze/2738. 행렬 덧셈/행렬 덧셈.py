01e9692ba0d43873e168b4eb33d2041971c52424
jaesuk0216 <113894609+jaesuk0216@users.noreply.github.com>
2022-10-31 19:30:00 +0900

    [Bronze V] Title: 행렬 덧셈, Time: 84 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
@@ -1,16 +1,16 @@
-A, B = [], []
+A,B = [], []
 
-N, M = map(int, input().split())
+N,M = map(int,input().split())
 
-for row in range(N):
-    row = list(map(int, input().split()))
+for row in range(N) :
+    row = list(map(int,input().split()))
     A.append(row)
 
-for row in range(N):
-    row = list(map(int, input().split()))
+for row in range(N) :
+    row = list(map(int,input().split()))
     B.append(row)
-    
-for row in range(N):
-    for col in range(M):
-        print(A[row][col] + B[row][col], end=' ')
+
+for row in range(N) :
+    for cal in range(M) :
+        print(A[row][cal] + B[row][cal], end = ' ')
     print()
