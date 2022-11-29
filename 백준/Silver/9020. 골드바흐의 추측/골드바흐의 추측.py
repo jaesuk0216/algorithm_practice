4b0f3b9b4a09a5bd0cb35f01cb9ec273705dcd53
jaesuk0216 <113894609+jaesuk0216@users.noreply.github.com>
2022-11-29 16:57:37 +0900

    [Silver II] Title: 골드바흐의 추측, Time: 676 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.py
@@ -0,0 +1,21 @@
+def prime(n) :
+    if n == 1 :
+        return False
+
+    for j in range(2, int(n**0.5)+1) :
+        if n % j ==0 :
+            return False
+    return True
+
+
+
+for _ in range(int(input())) :
+    num = int(input())
+    a,b = num // 2, num //2
+    while a > 0 :
+        if prime(a) and prime(b) :
+            print(a,b)
+            break
+        else :
+            a -= 1
+            b += 1
