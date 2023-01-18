c7be93147f3aa450a34b2795cc126970c615ba8a
jaesuk0216 <113894609+jaesuk0216@users.noreply.github.com>
2023-01-18 21:03:39 +0900

    [Silver II] Title: 참외밭, Time: 44 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Silver/2477. 참외밭/참외밭.py
@@ -0,0 +1,30 @@
+import sys
+
+k = int(sys.stdin.readline())
+
+height = []
+width = []
+total = []
+
+for i in range(6) :
+    dir, length = map(int,sys.stdin.readline().split())
+    if dir == 1 or dir == 2 :
+        width.append(length)
+        total.append(length)
+    else :
+        height.append(length)
+        total.append(length)
+
+bigbox = max(height) * max(width)
+
+maxhidx = total.index(max(height))
+maxwidx = total.index(max(width))
+
+small1 = abs(total[maxhidx-1] - total[(maxhidx-5 if maxhidx == 5 else maxhidx +1)])
+
+small2 = abs(total[maxwidx-1] - total[(maxwidx-5 if maxwidx == 5 else maxwidx +1)])
+
+area = bigbox - (small1*small2)
+
+print(area*k)
+
