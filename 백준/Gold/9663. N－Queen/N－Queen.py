700172647186273ca62c8818ce01b7078fa7144e
jaesuk0216 <113894609+jaesuk0216@users.noreply.github.com>
2023-03-01 21:53:45 +0900

    [Gold IV] Title: N-Queen, Time: 9664 ms, Memory: 129244 KB -BaekjoonHub

---
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -0,0 +1,36 @@
+import sys
+
+def queen(row):
+    if row == N:
+        global res
+        res += 1
+        return
+
+    for i in range(N):
+        if check[i]:
+            continue 
+
+        board[row] = i 
+
+        possible = True 
+        for j in range(row): 
+            
+            if board[row] == board[j] or (row - j == abs(board[row] - board[j])):
+                possible = False 
+                break 
+        
+        if possible:
+            check[i] = True 
+            queen(row+1)
+            check[i] = False 
+
+
+if __name__ == '__main__':
+    N = int(sys.stdin.readline())
+    check = [False for _ in range(N)] 
+    board = [0 for _ in range(N)] 
+    res = 0
+
+    queen(0)
+
+    print(res)
