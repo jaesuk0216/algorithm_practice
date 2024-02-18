113d3cfc5ec195ed2a3929d2b65e83705cb4f733
jaesuk0216 <113894609+jaesuk0216@users.noreply.github.com>
2024-02-18 23:14:11 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 744 ms, Memory: 93316 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
@@ -1,19 +1,14 @@
 import sys
 
-input = sys.stdin.readline
+s = sys.stdin.readline().rstrip()
+count = [[0] * 26]
 
-lst = list(input())
+q = int(sys.stdin.readline().rstrip())
+for i, ch in enumerate(s):
+    count.append(count[len(count) - 1][:])
+    count[i + 1][ord(ch) - 97] += 1
 
-time = int(input())
-
-
-
-for _ in range(time) :
-    cnt = 0
-    c,start,end = input().split()
-    start = int(start)
-    end = int(end)
-    for i in range(end-start+1) :
-        if c in lst[start+i] :
-            cnt+=1
-    print(cnt)
+for _ in range(q):
+    alpha, l, r = sys.stdin.readline().rstrip().split()
+    answer = count[int(r) + 1][ord(alpha) - 97] - count[int(l)][ord(alpha) - 97]
+    print(answer)
